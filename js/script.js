781abbec9749705830ba79dc00877d42dd798bba
Kajetan Blaszczyk <k.blaszczyyk@gmail.com>
2023-02-16 17:00:14 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let formElement = document.querySelector(".js-form");
+let currencyElement = document.querySelector(".js-currency");
+let amountElement = document.querySelector(".js-amount");
+let resultElement = document.querySelector(".js-result");
+
+let EUR = 4.79
+let USD = 4.47
+let GBP = 5.42
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+        case "EUR":
+            result = amount / EUR;
+            break;
+        case "USD":
+            result = amount / USD;
+            break;
+        case "GBP":
+            result = amount / GBP;
+            break;
+    }
+    resultElement.innerText = result.toFixed(2) + " " + currency;
+});
